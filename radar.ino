c890c9915cf615ee0fd421461447665397c6cdd9
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2025-12-30 16:34:49 +0200

    Add WiFi credentials and integrate WiFi access point setup in radar.ino

--- radar.ino
+++ radar.ino
@@ -1,4 +1,9 @@
+#include "wifiService.h"
 #include "ultrasonicService.h"
+
+// WIFI Credentials
+const char* WIFI_SSID = "RADAR-67";
+const char* WIFI_PASS = "67676767";
 
 const int TRIG_PIN = 18;
 const int ECHO_PIN = 19;
@@ -7,9 +12,12 @@ void setup() {
   Serial.begin(115200);
 
   registerUltrasonicSensor(TRIG_PIN, ECHO_PIN);
+  setupWifiAccessPoint(WIFI_SSID, WIFI_PASS);
 }
 
 void loop() {
+  loopWifiAccessPoint();
+
   // read the distance
   static unsigned long last = 0;
   if (millis() - last >= 200) {
