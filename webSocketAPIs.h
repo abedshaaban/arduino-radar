35ca124f99e58580f079f8e96d1b3dbe41d02d30
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2026-01-01 19:14:05 +0200

    Add new functions for servo rotation control in webSocketAPIs.h. Introduced methods to get and toggle servo rotation state, and set servo position, supporting enhanced control and integration with existing features.

--- webSocketAPIs.h
+++ webSocketAPIs.h
@@ -8,3 +8,6 @@ void broadcastDistance(float distanceCm, float angle);
 // Callback functions that radar.ino must implement
 bool getSystemState();
 bool toggleSystemState();
+bool getServoRotationEnabled();
+bool toggleServoRotation();
+void setServoPosition(float angle);
